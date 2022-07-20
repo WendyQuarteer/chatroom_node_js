1bae243a47bfacca1c3bbe43873002cd62c5e233
Wendy Quarteer <wendyquarteer@outlook.com>
2022-07-20 11:29:20 +0200

    messages sending to all works

--- Client/client.js
+++ Client/client.js
@@ -1,5 +1,25 @@
-let socket = io.connect();
+let socket = io.connect();//define your socket
 
-io.on('connection', (socket) => {
-    console.log('someone connected');
+const form = document.getElementById("chatForm");//store the form in a variable
+const target = document.getElementById("allMessages")//store the target in a variable
+
+form.addEventListener('submit', (event) => {//when a button is clicked
+    event.preventDefault();//prevent the page from reloading
+
+    const input =  document.getElementById('input')//store the input in a variable
+    const message = input.value;
+    console.log(message)
+
+    socket.emit('form', message);//do an emit to the server
+
+
+})
+socket.on('displayMessage', (message) => {//receive the message back from the server.
+    target.innerHTML += '<li>'+ message + '</li>';
 });
+
+
+
+
+
+
--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,36 @@
-//We will use express and http to make it easy to host our client
-const express = require('express');
-const http = require('http');
-
-//define our application
-const app = express();
-//give the path to our client
-const clientPath = `${__dirname}/../client`;
-//use express to host the client
-app.use(express.static(clientPath));
-//use http to serve the app that express provides
-const server = http.createServer(app);
-//get the server live
-const port = 8080;
-server.listen(port, () =>{
-    console.log("server running on "+ port);
+const express = require('express');//We will use express and http to make it easy to host our client
+const app = express();//define our application
+const http = require('http');//require http
+const clientPath = `${__dirname}/../client`;//give the path to our client
+const server = http.createServer(app);//use http to serve the app that express provides
+const port = 8080;//get the server live
+app.use(express.static(clientPath));//use express to host the client
+const io = require('socket.io')(server);//set-up socket.io in the server
+server.listen(port, () => {
+    console.log("server running on " + port);
 });
-//set-up socket.io in the server
-const io = require('socket.io')(server);
 
-//make a counter and make it go up every time someone connects
-let counter = 0;
-    io.on('connection', function(socket) {
-        console.log(counter+' someone connected');
-        counter++;
 
-        socket.on('disconnect', function() {
-            counter--;
-            console.log(counter+'Got disconnect!');
-        });
+let counter = 0;//make a counter to find the amount of connected users
+io.on('connection', function (socket) { ///Add 1 each time a user connects
+    console.log(counter + ' someone connected');
+    counter++;
+
+    socket.on('disconnect', function () {//remove 1 each time a user connects
+        counter--;
+        console.log(counter + 'Got disconnect!');
     });
+
+    socket.on('form', (message) =>{//observer that waits until the message "toAll" gets passed to the server
+        io.emit("displayMessage", (message));
+        console.log(message)
+    });
+});
+
+
+
+
+
+
 
 
